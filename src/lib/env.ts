7369e34a2a52b8c24ff0ef4281e049a5e81fddbc
Lukáš Peťko <luke.petko@gmail.com>
2025-10-16 21:33:51 +0200

    adding minio config

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,6 +2,11 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   DB_URL: z.string(),
+  S3_ENDPOINT: z.string(),
+  S3_PORT: z.coerce.number().int().positive(),
+  S3_ACCESS_KEY: z.string(),
+  S3_SECRET_KEY: z.string(),
+  S3_USE_SSL: z.coerce.boolean().default(false),
 });
 
 export type Env = z.infer<typeof envSchema>;
---
+++ src/lib/fileManagement.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import * as Minio from 'minio';
+import { ENV } from './env';
+
+dotenv.config({ quiet: true });
+
+export const minioClient = new Minio.Client({
+  endPoint: ENV.S3_ENDPOINT,
+  port: ENV.S3_PORT,
+  accessKey: ENV.S3_ACCESS_KEY,
+  secretKey: ENV.S3_SECRET_KEY,
+  useSSL: ENV.S3_USE_SSL,
+});
