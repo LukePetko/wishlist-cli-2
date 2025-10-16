652a882f1c2b361e96125e577c828f0f0ebd0a16
Lukáš Peťko <luke.petko@gmail.com>
2025-10-16 21:37:17 +0200

    removing dotenv package

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import { ENV } from '../lib/env';
 import * as schema from './schema';
-
-dotenv.config({ quiet: true });
 
 const pool = new Pool({
   connectionString: ENV.DB_URL,
--- src/lib/fileManagement.ts
+++ src/lib/fileManagement.ts
@@ -1,8 +1,5 @@
-import dotenv from 'dotenv';
 import * as Minio from 'minio';
 import { ENV } from './env';
-
-dotenv.config({ quiet: true });
 
 export const minioClient = new Minio.Client({
   endPoint: ENV.S3_ENDPOINT,
